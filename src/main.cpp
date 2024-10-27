6f2eb27dd58e4fa4980675794d63770f41e5f5e6
Angel <angel92.addcs@gmail.com>
2024-10-26 20:58:01 -0500

    Added collision detection for the screen bounds, Got some bouncing boxes and added the ability to throw boxes when holding click and moving the mouse

--- src/main.cpp
+++ src/main.cpp
@@ -11,17 +11,62 @@ int main()
     SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
     InitWindow(800, 600, "Physics!");
 
-    auto newCube = new PhysicsCube();
-    space.AddElement("cube", newCube);
+    // auto newCube = new PhysicsCube();
+    // newCube->mass = 0.1f;
+    // newCube->position = {0, 0};
+    // space.AddElement("cube", newCube);
+
+    int spawnedAmount = 0;
+
+    bool throwing = false;
+    Vector2 startPosition = GetMousePosition();
 
     while (!WindowShouldClose())
     {
+
+        if (IsCursorOnScreen())
+        {
+            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+            {
+                startPosition = GetMousePosition();
+                throwing = true;
+            }
+
+            if (throwing)
+            {
+                Vector2 currentPosition = GetMousePosition();
+                DrawLine(startPosition.x, startPosition.y, currentPosition.x, currentPosition.y, GREEN);
+
+                if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON))
+                {
+                    throwing = false;
+
+                    auto spawnedCube = new PhysicsCube();
+                    spawnedCube->position = startPosition;
+                    spawnedCube->size = {100, 100};
+                    spawnedCube->velocity = Vector2Scale(
+                        Vector2Subtract(startPosition, currentPosition),
+                        10
+                        );
+                    // spawnedCube->hasGravity = false;
+                    space.AddElement("spawned" + std::to_string(spawnedAmount), spawnedCube);
+                    spawnedAmount++;
+                }
+            }
+        }
+
         space.Update();
 
         BeginDrawing();
         ClearBackground(darkGreen);
-
         space.Draw();
+
+        int i = 0;
+        for (auto objects : space.GetChildren())
+        {
+            i++;
+            DrawText(objects->name.c_str(), 20, 20 + (i * 25), 20, ORANGE);
+        }
 
         EndDrawing();
     }
--- src/spaceObject.h
+++ src/spaceObject.h
@@ -12,6 +12,7 @@ class Object
 public:
 	string name = "";
 	Vector2 position;
+	Vector2 size;
 	float rotation = 0;
 
 	Object() {}
@@ -23,29 +24,111 @@ public:
 private:
 };
 
-class CubeObject : public Object
+const Vector2 gravity = {0, 10};
+
+class PhysicsObject : public Object
+{
+public:
+	Vector2 velocity;
+	Vector2 force;
+	float mass = 1.0f;
+	bool hasGravity = true;
+
+	// Returns the collision normal
+	Vector2 CheckScreenCollisions(Vector2 pos, Vector2 sizeBounds) const
+	{
+		Vector2 halfSize = {sizeBounds.x * 0.5f, sizeBounds.y * 0.5f};
+
+		int top 	= -((pos.y - halfSize.y) < 0);
+		int left 	= -((pos.x - halfSize.x) < 0);
+		int bottom 	= (pos.y + halfSize.y) > GetScreenHeight();
+		int right 	= (pos.x + halfSize.x) > GetScreenWidth();
+
+		return {
+			static_cast<float>(right + left),
+			static_cast<float>(top + bottom),
+		};
+	}
+
+	void Update() override
+	{
+
+		if (hasGravity)
+		{
+			force = Vector2Add(force, Vector2Scale(gravity, mass));
+		}
+
+		velocity = Vector2Add(velocity, Vector2Divide(force, {mass, mass}));
+
+		if (isinf(velocity.x) || isnan(velocity.x))
+		{
+			velocity.x = 0;
+		}
+
+		if (isinf(velocity.y) || isnan(velocity.y))
+		{
+			velocity.y = 0;
+		}
+
+		// cout << "x:" << velocity.x << " y:" << velocity.y << endl;
+		Vector2 newPosition = Vector2Add(position, Vector2Scale(velocity, GetFrameTime()));
+
+		Vector2 screenHitNormals = CheckScreenCollisions(newPosition, size);
+
+		if (screenHitNormals.x != 0 || screenHitNormals.y != 0)
+		{
+			Vector2 reflections = {
+				abs(screenHitNormals.x) == 1 ? -1 : 1, 
+				abs(screenHitNormals.y) == 1 ? -1 : 1
+			};
+
+			Vector2 newVelocity = {
+				velocity.x * reflections.x,
+				velocity.y * reflections.y,
+			};
+
+			Vector2 drag = {
+				screenHitNormals.x != 0 ? 0.99f : 1,
+				screenHitNormals.y != 0 ? 0.99f : 1,
+			};
+
+			velocity = Vector2Multiply(newVelocity, drag);
+
+			if (screenHitNormals.x == 0)
+			{
+				position.x = newPosition.x;
+			}
+
+			if (screenHitNormals.y == 0)
+			{
+				position.y = newPosition.y;
+			}
+
+			return;
+		}
+
+		position = newPosition;
+		force = {0, 0}; // Reset net force
+	}
+};
+
+class PhysicsCube : public PhysicsObject
 {
 public:
 	void Draw() override
 	{
-		DrawRectanglePro({0, 0, 100, 100}, position, rotation, ORANGE);
+		DrawRectanglePro({position.x, position.y, size.x, size.y}, {size.x * 0.5f, size.y * 0.5f}, rotation, ORANGE);
 	}
 };
 
-class PhysicsCube : public CubeObject
+class Cube : public Object
 {
 public:
-	Vector2 velocity;
-	bool hasGravity = true;
-
-	void Update() override
+	void Draw() override
 	{
-		Vector2 resultVelocity = Vector2Add(velocity, {
-			0.0f, hasGravity ? -100.0f : 0.0f
-		});
-		position = Vector2Add(position, Vector2Scale(resultVelocity, GetFrameTime()));
+		// cout << "hello coming from Cube { x:" << position.x << ", y:" << position.y << " }" << endl;
+		DrawRectanglePro({position.x, position.y, 100, 100}, {50, 50}, rotation, RED);
 	}
-
 };
 
 class ObjectManager
@@ -78,6 +161,11 @@ public:
 		element->name = name;
 		children.push_back(element);
 		return element;
+	}
+
+	vector<Object *> GetChildren()
+	{
+		return children;
 	}
 
 	void Update()
