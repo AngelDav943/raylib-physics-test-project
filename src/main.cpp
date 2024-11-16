a29c2a695718e3115a9a2da55cd1f2d74c37fa14
Angel <angel92.addcs@gmail.com>
2024-11-15 23:04:09 -0500

    App now opens and kinda works on android

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <raylib.h>
+#include <list>
 
 #include "spaceObject.h"
 
@@ -8,13 +9,18 @@ ObjectManager space;
 int main()
 {
     const Color darkGreen = {20, 160, 133, 255};
+#ifdef PLATFORM_DESKTOP
     SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT);
     InitWindow(800, 600, "Physics!");
+#else
+    InitWindow(0, 0, "Physics!");
+#endif
 
-    // auto newCube = new PhysicsCube();
-    // newCube->mass = 0.1f;
-    // newCube->position = {0, 0};
-    // space.AddElement("cube", newCube);
+    auto newCube = new PhysicsCube();
+    newCube->mass = 0.1f;
+    newCube->position = {0, 0};
+    newCube->velocity = {100, 100};
+    space.AddElement("cube", newCube);
 
     int spawnedAmount = 0;
 
@@ -23,21 +29,22 @@ int main()
 
     while (!WindowShouldClose())
     {
-
         if (IsCursorOnScreen())
         {
-            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+
+            // if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+            if (IsGestureDetected(GESTURE_TAP))
             {
-                startPosition = GetMousePosition();
+                startPosition = GetTouchPosition(0);
                 throwing = true;
             }
 
             if (throwing)
             {
-                Vector2 currentPosition = GetMousePosition();
+                Vector2 currentPosition = GetTouchPosition(0);
                 DrawLine(startPosition.x, startPosition.y, currentPosition.x, currentPosition.y, GREEN);
 
-                if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON))
+                if (IsGestureDetected(GESTURE_NONE))
                 {
                     throwing = false;
 
@@ -46,8 +53,7 @@ int main()
                     spawnedCube->size = {100, 100};
                     spawnedCube->velocity = Vector2Scale(
                         Vector2Subtract(startPosition, currentPosition),
-                        10
-                        );
+                        10);
                     // spawnedCube->hasGravity = false;
                     space.AddElement("spawned" + std::to_string(spawnedAmount), spawnedCube);
                     spawnedAmount++;
@@ -61,12 +67,35 @@ int main()
         ClearBackground(darkGreen);
         space.Draw();
 
+#ifdef PLATFORM_DESKTOP
+        DrawText("Running on Desktop", 10, 10, 20, BLUE);
+#else
+        DrawText("Running on Android", 10, 10, 20, RED);
+#endif
+
+
+        // if (GetTouchPointCount() > 0) {
+        Vector2 pos = GetTouchPosition(0);
+        list<string> texts = {
+            ("GESTURE: " + to_string(GetGestureDetected())),
+            ("X: " + to_string(pos.x)),
+            ("Y: " + to_string(pos.y)),
+        };
+
         int i = 0;
         for (auto objects : space.GetChildren())
         {
             i++;
-            DrawText(objects->name.c_str(), 20, 20 + (i * 25), 20, ORANGE);
+            DrawText(objects->name.c_str(), 20, 20 + (texts.size() * 25) + (i * 25), 20, BLUE);
         }
+
+        i = 0;
+        for (string text : texts)
+        {
+            i++;
+            DrawText(text.c_str(), 20, 20 + (i * 25), 20, ORANGE);
+        }
+        // }
 
         EndDrawing();
     }
